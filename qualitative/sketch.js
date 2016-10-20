8906e180e02090b107b4b7e53a87ac29d646fa40
Joshua Lee <neuralism@gmail.com>
2016-10-20 09:51:02 +0000

    Optimised performance and modified design

--- qualitative/sketch.js
+++ qualitative/sketch.js
@@ -1,5 +1,8 @@
 var content;
+var keyword;
 var tags = ['nn', 'nns', 'nnp', 'vb', 'vbn'];
+var topic = ['corruption', 'bribery', 'fraud', 'embezzlement', 'police', 'political', 'judicial'];
+var firstRun = true;
 
 function setup() {
     noCanvas();
@@ -19,37 +22,42 @@ function loadComplete(c) {
 }
 
 function show(k) {
-    var keyword = k.toLowerCase();
+    keyword = k.toLowerCase();
     for (var i in sentences) {
         var match = Boolean(String(sentences[i]).toLowerCase().match(keyword));
         if (match) createElement('div', sentences[i]).class('quote');
     }
-    var splitText = new SplitText('.quote', { type: 'words', wordsClass: 'word' });
     $('div.quote').each(function(index) {
-        TweenLite.from(this, 1, { opacity: 0, y: 25, ease: Power4.easeOut, delay: index * 0.05 + 0.25 });
+        TweenLite.from(this, 0.1, { onStart: splitQuote, onStartParams: [this], ease: Power4.easeOut, delay: index * 0.05 });
+        TweenLite.from(this, 0.5, { opacity: 0, y: 15, ease: Power4.easeOut, delay: index * 0.05 + 0.25 });
     });
-    $('div.word').each(function() {
+ }
+
+function splitQuote(quote) {
+    var st = new SplitText(quote, { type: 'words', wordsClass: 'word' });
+    $(quote).find('.word').each(function(index) {
         var word = new RiString($(this).text().toLowerCase().replace(/[,.%]/g, ''));
         var pos = word.pos();
         var match = Boolean(String(word).match(keyword));
+        if (match) {
+            $(this).addClass('keyword');
+            if (firstRun) TweenLite.from(this, 3, { color: '#3e3e3e', ease: Power4.easeOut, delay: 1  });
+        }        
         for (var i in pos) {
             pos[i] = String(pos[i]);
             for (var j in tags) {
-                if (pos[i] === tags[j] && !match) $(this).addClass('pos');
+                if (pos[i] === tags[j] && !match) {
+                    $(this).addClass('pos');
+                    if (firstRun) TweenLite.from(this, 3, { color: '#3e3e3e', ease: Power4.easeOut, delay: 2.25 });
+                }
             }
         }
-        if (match) $(this).addClass('keyword');
-    });
-    $('div.keyword').each(function() {
-        TweenLite.from(this, 2, { color: '#3e3e3e', ease: Power4.easeOut, delay: 1.1 });
-    });
-    $('div.pos').each(function() {
-        TweenLite.from(this, 2.2, { color: '#3e3e3e', ease: Power4.easeOut, delay: 2 });
     });
 }
 
 function update(event) {
     var keyword = event.currentTarget.name;
+    firstRun = false;
     $('div').remove();
     TweenLite.to($('a'), 0.5, { opacity: 0.4, ease: Power4.easeOut }); 
     TweenLite.to($('a[name=' + event.currentTarget.name + ']'), 0.5, { opacity: 1, ease: Power4.easeOut }); 
@@ -59,12 +67,20 @@ function update(event) {
 function search(event) {
     var keyword = String($('#search').val().toLowerCase());
     var match = Boolean(content.toLowerCase().match(keyword));
+    firstRun = false;
     $('div').remove();
     TweenLite.to($('a'), 0.5, { opacity: 0.4, ease: Power4.easeOut });
-    if (match && keyword !== '') {
+    for (var i in topic) {
+        if (keyword === topic[i]) {
+            TweenLite.to($('a'), 0.5, { opacity: 0.4, ease: Power4.easeOut }); 
+            TweenLite.to($('a.' + topic[i]), 0.01, { opacity: 1, ease: Power4.easeOut }); 
+        }
+    }        
+    if (match && keyword !== '' && keyword !== ' ') {
         createElement('div', 'Results for \"' + keyword + '\"').class('info');
         show(keyword);
-    } else {
-        createElement('div', 'No results for \"' + keyword + '\"').class('info');
+    } else { 
+        createElement('div', 'No results for \"' + keyword + '\"').class('info'); 
     }
+    
 }
